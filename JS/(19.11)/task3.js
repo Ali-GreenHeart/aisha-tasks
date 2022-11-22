edd6253f4c4e4817dedd6b4db35c19419b93670c
AishaNabi <aishanabiyeva0@gamil.com>
2022-11-22 10:28:19 +0400

    add 22.11

---
+++ JS/(19.11)/task3.js
@@ -0,0 +1,16 @@
+Number.prototype.isPrime = function(){
+    for(let i=2; i<Math.sqrt(this); i++){
+        if(this%i==0) return false
+    }
+    return true
+}
+Number.prototype.isEven = function() {
+    return this%2==0 ? true : false
+}
+Number.prototype.isInfinty = function() {
+    return this==Infinity ? true : false
+}
+let num = 2
+console.log(num.isPrime())
+console.log(num.isEven())
+console.log(num.isInfinty())
---
+++ JS/(19.11)/task4.js
@@ -0,0 +1,20 @@
+String.prototype.toCapitalize = function (){
+    return this[0].toUpperCase() + this.slice(1, this.lengh)
+}
+String.prototype.removeWhiteSpaces = function() {
+    return this.replace(" ", "")
+}
+String.prototype.filterDigits = function (){
+    let str = this.split("")
+    let a = []
+    str.forEach(elem =>{
+        if(!isNaN(elem)) a.push(elem)
+    }) 
+    return a
+}
+let str = "test"
+let str1 = "t est"
+let str2 = "t1e2s3t4"
+console.log(str1.removeWhiteSpaces())
+console.log(str.toCapitalize())
+console.log(str2.filterDigits())
